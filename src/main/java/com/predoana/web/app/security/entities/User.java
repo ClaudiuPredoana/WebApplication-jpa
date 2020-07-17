6dc4e9cfc5f21991a2da5719faf83fa74928351d
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-17 10:32:46 +0300

    Create a security related JPA entity

---
+++ src/main/java/com/predoana/web/app/security/entities/User.java
@@ -0,0 +1,50 @@
+package com.predoana.web.app.security.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    private boolean disabled;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+}
