0e27b9880506cc8b640215f67667311a0590a0e6
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-17 10:42:25 +0300

    Create an orders-related JPA entity

---
+++ src/main/java/com/predoana/web/app/orders/entities/Order.java
@@ -0,0 +1,41 @@
+package com.predoana.web.app.orders.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable=false, name="cust_name")
+    private String customerName;
+
+    @Column(nullable=false, name="cust_email")
+    private String customerEmail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+}
