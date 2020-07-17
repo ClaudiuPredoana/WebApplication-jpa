2ac4ac2d60d458e35eb6d2f727a4178f321b2037
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-17 10:46:22 +0300

    Create the security-data.sql script

--- src/main/resources/security-data.sql
+++ src/main/resources/security-data.sql
@@ -1,9 +1,11 @@
 
+delete from users;
+
 insert into users(id, name, email,disabled)
 values(1,'John','john@gmail.com', false);
 
 insert into users(id, name, email,disabled)
-values(2,'Rod','rod@gmail.com', false);
+values(2,'Rob','rob@gmail.com', false);
 
 insert into users(id, name, email,disabled)
-values(3,'Becky','becky@gmail.com', true);
+values(3,'Remo','remo@gmail.com', true);
